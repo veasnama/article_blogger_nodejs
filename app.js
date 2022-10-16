660ddab55cc9f8103ac76103c793b170a59892ed
Ma veasna <47025775+veasnama@users.noreply.github.com>
2022-10-16 19:56:47 +0700

    register form
    Finished register form

--- app.js
+++ app.js
@@ -2,30 +2,30 @@
 const express = require("express");
 const config = require("dotenv").config();
 const connectDB = require("./config/database");
-const path = require("path")
+const path = require("path");
 connectDB(process.env.LOCAL_MONGODB_URL);
 // create express server object
 const app = express();
 
-
-
 // Load View Engine
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "pug")
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 // set public static folder to express
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 // uses specific middleware
 // app.use()
 // home (default) route  when user nagivate to differ URL path
-// }) 
+// })
 
-let home = require("./routes/home")
-let article = require("./routes/article")
-app.use(article)
-app.use(home)
+let home = require("./routes/home");
+let article = require("./routes/article");
+let user = require("./routes/user");
+app.use(article);
+app.use(home);
+app.use(user);
 const port = 3000;
 // listen to specfic port  waiting for client request
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
